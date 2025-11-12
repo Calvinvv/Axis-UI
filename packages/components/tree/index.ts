c24c1eb8728a2948714d03f18eff7df3ee60192a
Calvin <calvinvwei@gmail.com>
2025-11-12 20:19:28 +0800

    fix: 修正tree组件类型声明中的组件名称

--- packages/components/tree/index.ts
+++ packages/components/tree/index.ts
@@ -8,7 +8,7 @@ export default Tree
 //引用模板后有组件注释了,代码里面的“AxTree”改成什么名字，实际项目中引入的什么标签就会被当成什么类型
 declare module 'vue' {
   export interface GlobalComponents {
-    AxTree: typeof _Tree
+    AxTree: typeof Tree
   }
 }
 
